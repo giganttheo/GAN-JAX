74469428895cf65a3aaa51a1097a0b34d2065f11
giganttheo <giganttheo@gmail.com>
2021-09-01 22:33:21 +0200

    merged losses and models in models

--- src/main.py
+++ src/main.py
@@ -38,7 +38,7 @@ def main():
             loss['generator'].append(batch_loss['generator'])
             loss['discriminator'].append(batch_loss['discriminator'])
     
-    sample = eval_step(optim_g.target, vars_g, latent, generator=Generator(training=False))
+    sample = eval_step(optim_g.target, vars_g, latent)
     plot(sample, loss, epoch)
 
 if __name__ == "main":
