5400e2d16b1d22804a76ae8e3ba302aadc8ab166
giganttheo <giganttheo@gmail.com>
2021-09-06 10:06:28 +0200

    Changed the model file for infogan and vanilla gan

--- src/main.py
+++ src/main.py
@@ -9,37 +9,11 @@ from utils import plot, plot_conditional, sample_latent
 
 def train_vanilla():
     key = jax.random.PRNGKey(seed=41)
-    key, key_gen, key_disc, key_latent = jax.random.split(key, 4)
-
     data_gen, batches_in_epoch = get_data()
+    from models.vanilla_gan import VanillaGan
+    models = VanillaGan()
+    models.train(data_gen, batches_in_epoch, key)
 
-    # Retrieve shapes for generator and discriminator input.
-    latent = sample_latent(key_latent, shape=(100, 64))
-    image_shape = next(data_gen).shape
-
-    # Generate initial variables (parameters and batch statistics).
-    vars_g = Generator().init(key_gen, jnp.ones(latent.shape, jnp.float32))
-    vars_d = Discriminator().init(key_disc, jnp.ones(image_shape, jnp.float32))
-
-    # Create optimizers.
-    optim_g = flax.optim.Adam(0.0002, 0.5, 0.999).create(vars_g['params'])
-    optim_d = flax.optim.Adam(0.0002, 0.5, 0.999).create(vars_d['params'])
-
-    loss = {'generator': [], 'discriminator': []}
-
-    for epoch in range(1, 51):
-        for batch in range(batches_in_epoch):
-            data = next(data_gen)
-
-            batch_loss, vars_g, vars_d, optim_g, optim_d, key = train_step(
-                data, vars_g, vars_d, optim_g, optim_d, key
-            )
-
-            loss['generator'].append(batch_loss['generator'])
-            loss['discriminator'].append(batch_loss['discriminator'])
-    
-    sample = eval_step(optim_g.target, vars_g, latent)
-    plot(sample, loss, epoch)
 
 def train_wgan():
     data_gen, batches_in_epoch = get_data()
@@ -51,80 +25,17 @@ def train_wgan():
 
 def train_conditional_gan():
     data_gen, batches_in_epoch = get_data()
-
     key = jax.random.PRNGKey(seed=41)
-    key, key_gen, key_disc, key_latent = jax.random.split(key, 4)
-
-    # Retrieve shapes for generator and discriminator input.
-    latent = sample_latent(key_latent, shape=(100, 64))
-    labels = jnp.tile(jnp.arange(10), 10)
-    oh_labels = jax.nn.one_hot(labels, 10)
-    latent = jnp.concatenate([latent, oh_labels], axis=-1)
-
-    image_shape = next(data_gen)[0].shape[:3] + (11,)
-
-    # Generate initial variables (parameters and batch statistics).
-    vars_g = Generator().init(key_gen, jnp.ones(latent.shape, jnp.float32))
-    vars_d = Discriminator().init(key_disc, jnp.ones(image_shape, jnp.float32))
-
-    # Create optimizers.
-    optim_g = flax.optim.Adam(0.0002, 0.5, 0.999).create(vars_g['params'])
-    optim_d = flax.optim.Adam(0.0002, 0.5, 0.999).create(vars_d['params'])
-
-    loss = {'generator': [], 'discriminator': []}
-
-    for epoch in range(1, 51):
-      for batch in range(batches_in_epoch):
-        data = next(data_gen)
-
-        batch_loss, vars_g, vars_d, optim_g, optim_d, key = train_step(
-            data, vars_g, vars_d, optim_g, optim_d, key
-        )
-
-        loss['generator'].append(batch_loss['generator'])
-        loss['discriminator'].append(batch_loss['discriminator'])
-      
-      sample = eval_step(optim_g.target, vars_g, latent)
-      plot_conditional(sample, loss, labels, epoch)
-
+    from models.conditional_gan import ConditionalGan
+    models = ConditionalGan()
+    models.train(data_gen, batches_in_epoch, key)
 
 def train_infogan():
     data_gen, batches_in_epoch = get_data()
-
-    
     key = jax.random.PRNGKey(seed=41)
-    key, key_gen, key_disc, key_latent = jax.random.split(key, 4)
-
-    # Retrieve shapes for generator and discriminator input.
-    noise = jax.random.normal(key, (100, 64))
-    c = jnp.tile(jnp.arange(10), 10)
-    c = jax.nn.one_hot(c, 10)
-    latent = jnp.concatenate([noise, c], axis=-1)
-    image_shape = next(data_gen).shape
-
-    # Generate initial variables (parameters and batch statistics).
-    vars_g = Generator().init(key_gen, jnp.ones(latent.shape, jnp.float32))
-    vars_d = Discriminator().init(key_disc, jnp.ones(image_shape, jnp.float32))
-
-    # Create optimizers.
-    optim_g = flax.optim.Adam(0.0001, 0.5, 0.999).create(vars_g['params'])
-    optim_d = flax.optim.Adam(0.0002, 0.5, 0.999).create(vars_d['params'])
-
-    loss = {'generator': [], 'discriminator': []}
-
-    for epoch in range(1, 101):
-      for batch in range(batches_in_epoch):
-        data = next(data_gen)
-
-        batch_loss, vars_g, vars_d, optim_g, optim_d, key = train_step(
-            data, vars_g, vars_d, optim_g, optim_d, key
-        )
-
-        loss['generator'].append(batch_loss['generator'])
-        loss['discriminator'].append(batch_loss['discriminator'])
-      
-      sample = eval_step(optim_g.target, vars_g, latent)
-      plot(sample, loss, epoch)
+    from models.infogan import InfoGan
+    models = InfoGan()
+    models.train(data_gen, batches_in_epoch, key)
 
 def main():
     pass
--- src/models/infogan.py
+++ src/models/infogan.py
@@ -1,9 +1,12 @@
 import jax
 import jax.numpy as jnp
+import flax
 from flax import linen as nn
 from utils import sample_latent_categorical as sample_latent
+from utils import plot
 from architecture.dcgan import Generator
 from architecture.dcgan import DiscriminatorAndRecognitionNetwork as Discriminator
+from models.base_model import Model
 
 #Losses
 
@@ -113,3 +116,44 @@ def eval_step(params, vars, latent):
   )
 
   return fake_data
+
+
+#Training loop
+
+class InfoGan(Model):
+
+    def train(self, data_gen, batches_in_epoch, key, verbose=1):
+        epochs = 101
+
+        key, key_gen, key_disc, key_latent = jax.random.split(key, 4)
+
+        # Retrieve shapes for generator and discriminator input.
+        noise = jax.random.normal(key, (100, 64))
+        c = jnp.tile(jnp.arange(10), 10)
+        c = jax.nn.one_hot(c, 10)
+        latent = jnp.concatenate([noise, c], axis=-1)
+        image_shape = next(data_gen).shape
+
+        # Generate initial variables (parameters and batch statistics).
+        vars_g = Generator().init(key_gen, jnp.ones(latent.shape, jnp.float32))
+        vars_d = Discriminator().init(key_disc, jnp.ones(image_shape, jnp.float32))
+
+        # Create optimizers.
+        optim_g = flax.optim.Adam(0.0001, 0.5, 0.999).create(vars_g['params'])
+        optim_d = flax.optim.Adam(0.0002, 0.5, 0.999).create(vars_d['params'])
+
+        loss = {'generator': [], 'discriminator': []}
+
+        for epoch in range(1, epochs):
+            for batch in range(batches_in_epoch):
+                data = next(data_gen)
+
+                batch_loss, vars_g, vars_d, optim_g, optim_d, key = train_step(
+                    data, vars_g, vars_d, optim_g, optim_d, key
+                )
+
+                loss['generator'].append(batch_loss['generator'])
+                loss['discriminator'].append(batch_loss['discriminator'])
+            
+                sample = eval_step(optim_g.target, vars_g, latent)
+                plot(sample, loss, epoch)
